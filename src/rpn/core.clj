0567e1133d300c2b62fdd4d0d5a84770a9228c42
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-01-15 20:54:26 +0100

    refactor - introduce stack

--- src/rpn/core.clj
+++ src/rpn/core.clj
@@ -1,12 +1,12 @@
 (ns rpn.core
   (:require [clojure.string :as str]))
 
-(defn- parse-token [token]
+(defn- process-token [stack token]
   (let [object (read-string token)]
     (if (number? object)
-      (str object)
+      (cons object stack)
       (throw (Exception. "Unable to parse input")))))
 
 (defn calculator [expression]
   (let [tokens (str/split (str/trim expression) #"\s+")]
-    (str/join " " (map parse-token tokens))))
+    (str/join " " (reverse (reduce process-token nil tokens)))))
