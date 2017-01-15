f4d24f8cb649f775abdd775dcba22d56d1f4132b
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-01-15 11:17:28 +0100

    first commit

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject rpn "0.0.1-SNAPSHOT"
+  :description "Reverse Polish Notation Calculator Kata"
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :profiles {:dev {:dependencies [[midje "1.5.1"]]}})
+  
