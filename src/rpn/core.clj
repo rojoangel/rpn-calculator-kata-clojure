fcb68a66cb99d0b765c5661b02f63ab9d0805f5d
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-01-15 22:15:44 +0100

    refactor - extract process-number and process-operation private functions

--- src/rpn/core.clj
+++ src/rpn/core.clj
@@ -6,14 +6,20 @@
                  (symbol "*") (resolve (symbol "*"))
                  (symbol "/") (resolve (symbol "/"))})
 
+(defn- process-number [number stack]
+  (cons number stack))
+
+(defn- process-operation [operation stack]
+  (if (>= (count stack) 2)
+    (cons (operation (first (rest stack)) (first stack)) (drop 2 stack))
+    (throw (Exception. "Unexpected operator"))))
+
 (defn- process-token [stack token]
   (let [object (read-string token)]
     (if (number? object)
-      (cons object stack)
+      (process-number object stack)
       (if-let [operation (object operations)]
-        (if (>= (count stack) 2)
-          (cons (operation (first (rest stack)) (first stack)) (drop 2 stack))
-          (throw (Exception. "Unexpected operator")))
+        (process-operation operation stack)
         (throw (Exception. "Unable to parse input"))))))
 
 (defn calculator [expression]
