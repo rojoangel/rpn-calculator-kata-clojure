535adaa28135e0bac456e970a9fe50d16e4b0271
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-01-15 21:56:43 +0100

    refactor - add operations map to reduce duplicity

--- src/rpn/core.clj
+++ src/rpn/core.clj
@@ -1,20 +1,18 @@
 (ns rpn.core
   (:require [clojure.string :as str]))
 
+(def operations {(symbol "+") (resolve (symbol "+"))
+                 (symbol "-") (resolve (symbol "-"))})
 
 (defn- process-token [stack token]
   (let [object (read-string token)]
     (if (number? object)
       (cons object stack)
-      (if (= object '+)
+      (if-let [operation (object operations)]
         (if (>= (count stack) 2)
-          (cons (+ (first (rest stack)) (first stack)) (drop 2 stack))
+          (cons (operation (first (rest stack)) (first stack)) (drop 2 stack))
           (throw (Exception. "Unexpected operator")))
-        (if (= object '-)
-          (if (>= (count stack) 2)
-            (cons (- (first (rest stack)) (first stack)) (drop 2 stack))
-            (throw (Exception. "Unexpected operator")))
-          (throw (Exception. "Unable to parse input")))))))
+        (throw (Exception. "Unable to parse input"))))))
 
 (defn calculator [expression]
   (let [tokens (str/split (str/trim expression) #"\s+")]
