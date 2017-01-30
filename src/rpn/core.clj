3fc332a6d12f499756ce3ab05445d129257ae27c
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-01-30 21:33:27 +0100

    refactor: change visibility to private for 'to-symbols' and 'process-operation'

--- src/rpn/core.clj
+++ src/rpn/core.clj
@@ -6,14 +6,14 @@
                  '* *
                  '/ /})
 
-(defn to-symbols [expression]
+(defn- to-symbols [expression]
   (let [tokens (str/split (str/trim expression) #"\s+")]
     (map #(read-string %) tokens)))
 
 (defn- process-number [number stack]
   (cons number stack))
 
-(defn process-operation [operation stack]
+(defn- process-operation [operation stack]
   (cons (operation (first (rest stack)) (first stack)) (drop 2 stack)))
 
 (defn- process-token [stack token]
