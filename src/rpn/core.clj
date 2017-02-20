94fff0325d6cea037654f140907bd2a6981c6ec2
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-02-21 00:02:41 +0100

    add :pre conditions and tests asserting on AssertionError

--- src/rpn/core.clj
+++ src/rpn/core.clj
@@ -6,15 +6,23 @@
                  '* *
                  '/ /})
 
+(defn- filter-out-unsupported-symbols [symbols]
+  (filter #(or (number? %) (% operations)) symbols))
+
+(defn- validate-symbols [symbols]
+  {:pre [(= symbols (filter-out-unsupported-symbols symbols))]}
+  symbols)
+
 (defn- to-symbols [expression]
   (let [tokens (str/split (str/trim expression) #"\s+")
         symbols (map #(read-string %) tokens)]
-    symbols))
+    (validate-symbols symbols)))
 
 (defn- process-number [number stack]
   (cons number stack))
 
 (defn- process-operation [operation stack]
+  {:pre [(>= (count stack) 2)]}
   (cons (operation (first (rest stack)) (first stack)) (drop 2 stack)))
 
 (defn- process-token [stack token]
--- test/rpn/core_test.clj
+++ test/rpn/core_test.clj
@@ -6,25 +6,49 @@
        (fact "When a digit is sent it should display the same digit"
              (calculator "1") => "1"
              (calculator "99") => "99")
+       (fact "When an alpha character is sent it throws an error"
+             (calculator "a") => (throws AssertionError)
+             (calculator "Z") => (throws AssertionError)
+             (calculator "P") => (throws AssertionError))
        (fact "When some digits are sent it should display the number formed by those digits"
              (calculator "1 2") => "1 2"
              (calculator " 1 2 ") => "1 2")
+       (fact "When the expression constains an alpha it throws an error"
+             (calculator "1 a") => (throws AssertionError)
+             (calculator "a 1") => (throws AssertionError)
+             (calculator " 1 a ") => (throws AssertionError))
        (fact "When a + operation is sent after two numbers it should display the result"
              (calculator "1 2 +") => "3"
              (calculator "1 2 + 3 +") => "6"
              (calculator "1 2 + 3 + 4 +") => "10")
+       (fact "When a + operation is not sent after two numbers it throws an error"
+             (calculator "+") => (throws AssertionError)
+             (calculator "2 +") => (throws AssertionError)
+             (calculator "1 + 2") => (throws AssertionError))
        (fact "When a - operation is sent after two numbers it should display the result"
              (calculator "2 1 -") => "1"
              (calculator "9 2 - 1 -") => "6"
              (calculator "8 1 - 4 - 1 -") => "2")
+       (fact "When a - operation is not sent after two numbers it throws an error"
+             (calculator "-") => (throws AssertionError)
+             (calculator "2 -") => (throws AssertionError)
+             (calculator "2 - 1") => (throws AssertionError))
        (fact "When a * operation is sent after two numbers it should display the result"
              (calculator "3 2 *") => "6"
              (calculator "9 2 * 3 *") => "54"
              (calculator "8 1 * 4 * 3 *") => "96")
+       (fact "When a * operation is not sent after two numbers it throws an error"
+             (calculator "*") => (throws AssertionError)
+             (calculator "2 *") => (throws AssertionError)
+             (calculator "2 * 1") => (throws AssertionError))
        (fact "When a / operation is sent after two numbers it should display the result"
              (calculator "8 2 /") => "4"
              (calculator "8 2 / 2 /") => "2"
              (calculator "8 2 / 2 / 2 /") => "1")
+       (fact "When a / operation is not sent after two numbers it throws an error"
+             (calculator "/") => (throws AssertionError)
+             (calculator "2 /") => (throws AssertionError)
+             (calculator "2 / 1") => (throws AssertionError))
        (fact "Works for the examples"
              (calculator "20 5 /") => "4"
              (calculator "4 2 + 3 -") => "3"
